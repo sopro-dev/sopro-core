afeb6cf33fd23f04ff7ae4d7deba482c46658e68
Pablo Diaz <pablo.diazv@pucp.edu.pe>
2023-01-08 11:36:55 -0400

    downsampler

--- sopro/downsampler/downsampler.go
+++ sopro/downsampler/downsampler.go
@@ -1,3 +1,20 @@
 package downsampler
 
+import "errors"
+
 // TODO: Add different downsampling methods
+
+// Decimation by factor
+func DownsamplerDecimationByFactor[T int16](in []T, factor int) ([]T, error) {
+	if factor <= 0 {
+		return nil, errors.New("downsampling factor must be positive")
+	}
+	if factor == 1 {
+		return in, nil
+	}
+	out := make([]T, len(in)/factor)
+	for i := 0; i < len(out); i++ {
+		out[i] = in[i*factor]
+	}
+	return out, nil
+}
