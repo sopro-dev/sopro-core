77658c353ef8397783613be6643d92cdea7a0c99
Pablo Diaz <pablo.diazv@pucp.edu.pe>
2023-01-27 12:53:38 -0400

    closing file properly

--- pkg/transcoder/mulaw2wav.go
+++ pkg/transcoder/mulaw2wav.go
@@ -96,6 +96,7 @@ func mulaw2WavLpcm(in *AudioFileIn, out *AudioFileOut, transcoder *Transcoder) (
 
 	// Update the file size and data size fields
 	fileFixer := out.Data.(*os.File)
+	defer fileFixer.Close()
 	r, err := fileFixer.Seek(4, io.SeekStart)
 	if err != nil {
 		return fmt.Errorf("error seeking file: %v", err)
