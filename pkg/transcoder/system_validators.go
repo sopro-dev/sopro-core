3ee96b8f0ddfe487a3127ef39e7c1b4d64921c49
Pablo Diaz <pablo.diazv@pucp.edu.pe>
2023-01-28 18:32:47 -0400

    removing empty validators

--- pkg/transcoder/system_validators.go
+++ pkg/transcoder/system_validators.go
@@ -1,7 +1,5 @@
 package transcoder
 
-import "github.com/pablodz/sopro/pkg/sopro"
-
 // TODO: add functions to validate the input and output configuration
 // ex. validate formats
 // ex. validate sample rate
@@ -12,14 +10,14 @@ import "github.com/pablodz/sopro/pkg/sopro"
 // ex. validate file size
 // validate not 1-1 transcoding inputs as 8k output as 16k
 
-func ValidateInput(in *sopro.In) error {
-	return nil
-}
+// func ValidateInput(in *sopro.In) error {
+// 	return nil
+// }
 
-func ValidateOutput(out *sopro.Out) error {
-	return nil
-}
+// func ValidateOutput(out *sopro.Out) error {
+// 	return nil
+// }
 
-func ValidateTranscoder(tr *Transcoder) error {
-	return nil
-}
+// func ValidateTranscoder(tr *Transcoder) error {
+// 	return nil
+// }
